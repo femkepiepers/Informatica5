50b0e38016c3d2ec92380b9c62f6e2149efdc76c
femkepiepers <43027739+femkepiepers@users.noreply.github.com>
2018-10-11 10:03:25 +0200

    Paradox

---
+++ 06- Condities/Paradox.py
@@ -0,0 +1,12 @@
+# invoer
+zenuwen = input('Wie heeft stalen zenuwen? ')
+
+#uitvoer
+if zenuwen == 'vader':
+    print('Krokodil geeft kind terug')
+
+if zenuwen == 'krokodil':
+    print('Krokodil eet kind op')
+
+if zenuwen != 'vader' and zenuwen != 'krokodil':
+    print('Moe van het denken')
