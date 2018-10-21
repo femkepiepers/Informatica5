23814aeac43ff538e60102829d8bbce5764334e3
femkepiepers <43027739+femkepiepers@users.noreply.github.com>
2018-10-21 17:51:07 +0200

    Risk

--- 06- Condities/Risk.py
+++ 06- Condities/Risk.py
@@ -1,9 +1,21 @@
 # invoer
-a1 = input('Aantal ogen a1: ')
-a2 = input('Aantal ogen a2: ')
-a3 = input('Aantal ogen a3: ')
-v1 = input('Aantal ogen v1: ')
-v2 = input('Aantal ogen v2: ')
+a = float(input('Aantal ogen dobbelsteen 1 aanvaller: '))
+b = float(input('Aantal ogen dobbelsteen 2 aanvaller: '))
+c = float(input('Aantal ogen dobbelsteen 3 aanvaller: '))
+d = float(input('Aantal ogen dobbelsteen 1 verdediger: '))
+e = float(input('Aantal ogen dobbelsteen 2 verdediger: '))
+
+# berekening
+f = max(a, b, c)
+h = min(a, b, c)
+i = max(d, e)
+j = a + b + c - f - h
+k = d + e - i
 
 # uitvoer
-if a1 > v1
+if f > i and j > k:
+    print('aanvaller verliest 0 legers, verdediger verliest 2 legers')
+if f <= i and j <= k:
+    print('aanvaller verliest 2 legers, verdediger verliest 0 legers')
+if f <= i and j > k or f > i and j <= k:
+    print('aanvaller verliest 1 leger, verdediger verliest 1 leger')
