7878c30a2d89610917e966f8051dc1dee49daa67
femkepiepers <43027739+femkepiepers@users.noreply.github.com>
2018-11-11 15:27:47 +0100

    Het grootste en het gemiddelde

--- 07a-Iteraties-For-lus/Het grootste en het gemiddelde.py
+++ 07a-Iteraties-For-lus/Het grootste en het gemiddelde.py
@@ -1,2 +1,15 @@
 # invoer
 aantal_getallen = int(input('aantal getallen: '))
+getal = ''
+som = ''
+
+for i in range(aantal_getallen):
+    getal = input('Getal: ')
+
+# Berekening
+max = max(getal)
+som += getal
+#gemiddelde = ((int(som)) / (int(aantal_getallen)))
+
+#uitvoer
+print(max, som)
