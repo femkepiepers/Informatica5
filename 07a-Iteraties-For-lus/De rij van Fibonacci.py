ee908c828f9d753d63c00f9450f8f246c4e294fe
femkepiepers <43027739+femkepiepers@users.noreply.github.com>
2018-11-12 20:56:06 +0100

    De rij van Fibonacci

---
+++ 07a-Iteraties-For-lus/De rij van Fibonacci.py
@@ -0,0 +1,12 @@
+# invoer
+getal = int(input('Getal uit de rij van Fibonacci: '))
+getal_1, getal_2, = 1, 1
+
+if getal > 2:
+    for i in range(3, getal + 1):
+         getal_3 = getal_1 + getal_2
+         getal_1 = getal_2
+         getal_2 = getal_3
+    print(getal_3)
+elif getal == 1 or getal ==2:
+    print('1')
