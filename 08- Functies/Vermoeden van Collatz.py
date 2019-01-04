9f8ea11f862ce5946f0c4e911cf58034cb5cc784
femkepiepers <43027739+femkepiepers@users.noreply.github.com>
2019-01-04 13:31:49 +0100

    Vermoeden van Collatz

--- 08- Functies/Vermoeden van Collatz.py
+++ 08- Functies/Vermoeden van Collatz.py
@@ -1,10 +1,23 @@
-def volgend_collatz_getal(n):
-    if n % 2 == 0:
-        volgend = n // 2
-    else:
-        volgend = (n * 3) + 1
-    return volgend
+# input
+getal = int(input('Getal: '))
+volgend_getal = ''
+cyclelengte = 0
 
-print(volgend_collatz_getal(88))
+# volgend getal berekenen
+if getal % 2 == 0:
+    volgend_getal = getal // 2
+elif getal % 2 != 0:
+    volgend_getal = (getal * 3) + 1
 
-def cyclelengte
+# cyclelengte berekenen
+while volgend_getal != 1:
+    if getal % 2 == 0:
+        volgend_getal = getal // 2
+
+    elif getal % 2 != 0:
+        volgend_getal = (getal * 3) + 1
+    cyclelengte += 1
+
+# uitvoer
+print(volgend_getal)
+print(cyclelengte)
