a4bcc9f282c650c70ab274e6371b9e426cd2b6bb
femkepiepers <43027739+femkepiepers@users.noreply.github.com>
2019-01-14 20:13:09 +0100

    Lingo

--- 10 - Strings/Lingo.py
+++ 10 - Strings/Lingo.py
@@ -1,21 +1,11 @@
 def hint(gok, woord):
-    antw = '.....'
-    if gok[0] == woord[0]:
-        antw = antw.replace([0], [0].upper())
-    elif gok[1] == woord[1]:
-        antw = antw.replace([1], [1].upper())
-    elif gok[2] == woord[2]:
-        antw = antw.replace([2], [2].upper())
-    elif gok[3] == woord[3]:
-        antw = antw.replace([3], [3].upper())
-    elif gok[4] == woord[4]:
-        antw = antw.replace([4], [4].upper())
-    elif gok[:] in woord[:]:
-        antw : antw.replace()
-
-
-
-
-
-
+    antw = ''
+    for i in range(0, 5):
+        if gok[i] in woord:
+            if gok[i] == woord[i]:
+                antw += gok[i].upper()
+            else:
+                antw += gok[i]
+        else:
+            antw += '.'
     return antw
