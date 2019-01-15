cd79a4149bcb3fa2cc73ae94ef2b923962141a6f
femkepiepers <43027739+femkepiepers@users.noreply.github.com>
2019-01-15 18:57:16 +0100

    Alfabetische woorden

--- 10 - Strings/Alfabetische woorden.py
+++ 10 - Strings/Alfabetische woorden.py
@@ -1,7 +1,29 @@
 def positie_laagste_ascii(woord):
-    mes = ''
+    a = 10000
     for i in range(0, len(woord)):
-        a = ord(woord[i])
-        if a > 32:
-            mes = i
+        if ord(woord[i]) < a:
+            a = ord(woord[i])
+    b = chr(a)
+    mes = woord.find(b)
+    return mes
+
+def sorteer(woord):
+    nieuwe_woord = ''
+    for i in range(0, len(woord)):
+        a = 10000
+        for i in range(0, len(woord)):
+            if ord(woord[i]) < a:
+                a = ord(woord[i])
+        b = chr(a)
+        nieuwe_woord += b
+        z = woord.find(b)
+        woord = woord[0:z] + woord[z + 1:]
+    return nieuwe_woord
+
+def is_alfabetisch(woord):
+    b = sorteer(woord)
+    if b == woord:
+        mes = True
+    else:
+        mes = False
     return mes
