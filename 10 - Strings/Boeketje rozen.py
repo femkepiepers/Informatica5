d16dbe4fc9e58fddbe7921568de963f128b03c7b
femkepiepers <43027739+femkepiepers@users.noreply.github.com>
2019-01-23 20:06:01 +0100

    Boeketje rozen

---
+++ 10 - Strings/Boeketje rozen.py
@@ -0,0 +1,7 @@
+# INVOER
+rw_rozen = int(input('Aantal rode en witte rozen? '))
+wb_rozen = int(input('Aantal witte en blauwe rozen? '))
+br_vs_wb = int(input('#blauwe & rode rozen groter/kleiner dan #witten & blauwe rozen? '))
+
+# BEREKENINGEN
+if
