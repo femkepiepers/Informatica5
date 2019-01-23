a5f5c6733ed25d61bc43dfe5399c692462000601
femkepiepers <43027739+femkepiepers@users.noreply.github.com>
2019-01-23 20:05:42 +0100

    Times Square

--- 10 - Strings/Times Square.py
+++ 10 - Strings/Times Square.py
@@ -6,4 +6,6 @@ def bereken_prijs(boodschap):
             if a > i:
                 mes += boodschap[0:i - 1] + ',' + boodschap[i + 1:a - 1] + boodschap[-1]
     return mes
+
+
 print(bereken_prijs('I spent my last money on this billboard. Please give me a job.<2.68>'))
