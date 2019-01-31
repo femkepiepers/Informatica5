196da69a9fedcbda6212c410ed846d5642928fe4
femkepiepers <43027739+femkepiepers@users.noreply.github.com>
2019-01-31 08:42:50 +0100

    Verbetering cryptografei voor dummies

--- Toets/Verbetering cryptografie voor dummies.py
+++ Toets/Verbetering cryptografie voor dummies.py
@@ -1,15 +1,33 @@
-def versleutel_woord(woord):
+# elke letter verschuift n letters in het ASCII-tabel
+# indien de nieuwe letter een @ is, verander dan het teken naar een spatie
 
-    versleuteld_woord = ''
-
+def versleutel_woord(woord, n):
+    code = ''
     woord = woord.upper()
-
     for letter in woord:
+        code_letter = chr(ord(letter) + n)
 
-        versleutelde_letter = chr(ord(letter) + n)
-        if versleutelde_letter == '@':
-            versleutelde_letter = ' '
+        if code_letter == '@':
+            code_letter = ' '
 
-        versleuteld_woord += versleutelde_letter
+        code_letter += code_letter
 
-    return versleuteld_woord
+    return code
+
+# elke woord in de zin wordt versleuteld.
+def versleutel_zin(zin, getal):
+
+    index_spatie = zin.find()
+    code = ''
+
+    while index_spatie != -1:
+        woord = zin[:index_spatie]
+        zin = zin[index_spatie + 1:]
+
+        code += '@' + versleutel_woord(woord, getal)
+        index_spatie = zin.find(' ')
+
+    if len(zin) > 0:
+        code += '@' + versleutel_woord(zin, getal)
+
+    return code
