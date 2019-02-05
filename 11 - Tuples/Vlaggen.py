d9fc6181996adbff74aafc8e063fcfb3e8b29529
femkepiepers <43027739+femkepiepers@users.noreply.github.com>
2019-02-05 21:00:04 +0100

    Vlaggen

--- 11 - Tuples/Vlaggen.py
+++ 11 - Tuples/Vlaggen.py
@@ -1,20 +1,18 @@
 def vlag(richting, kleuren):
 
     if richting == 'verticaal':
-        a = len(kleuren)
         mes = ''
-        for i in range(0, a + 1):
-            mes += kleuren[i] + ' | '
-            if i == a:
-                mes += kleuren[i]
+        for i in range(0, len(kleuren)):
+            mes += kleuren[i]
+            if i != len(kleuren) - 1:
+                mes += ' | '
+    elif richting == 'horizontaal':
+        mes = ''
+        for i in range(0, len(kleuren)):
+            mes += kleuren[i]
+            if i != len(kleuren) - 1:
+                mes += '\n' + '-' + '\n'
 
-        return mes
-
-   # elif richting == 'horizontaal':
-    #    for i in range(0, len(kleuren)):
-     #       mes = kleuren[i] + '\n' + '-' + '\n'
-
-      #  return mes
-
+    return mes
 
 print(vlag('verticaal',('rood', 'wit', 'blauw')))
