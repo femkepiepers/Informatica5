118d4d60aaf9ed6db0d06c1dd51f659250d55a34
femkepiepers <43027739+femkepiepers@users.noreply.github.com>
2019-02-14 09:38:07 +0100

    Snoepwinkel

---
+++ 12b - Roosters/Snoepwinkel.py
@@ -0,0 +1,37 @@
+def bereken_prijs(lijst):
+
+    prijs = 0
+
+    # overloop alle boodschapen
+    for item in lijst:
+
+        # telkens het element op index 1 optellen bij de totale prijs
+        prijs += item[1]
+
+    return prijs
+
+def winkelbriefje(lijst):
+
+    # nieuwe lijst maken
+    briefje = []
+
+    # overloop alle boodschappen
+    for item in lijst:
+        # telkens het element op index 0 aan de nieuwe lijst toevoegen
+        briefje.append(item[0])
+
+    return briefje
+
+def sorteer(lijst):
+
+    from operator import itemgetter
+    # overloop alle boodschappen
+    for item in lijst:
+
+        # sorteren op prijs, van goedkoop naar duur
+        lijst.sort(key = itemgetter(1))
+
+    return lijst
+
+
+print(bereken_prijs([('Lays Paprika', 3.94), ('Napoleon', 1.48), ('Milky Way', 3.64), ('M&M', 3.06), ('Crocky Zout', 3.62), ('Bounty', 1.86)]))
