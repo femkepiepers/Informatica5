417d5c430fd199dc9a92ed44bf91ab802e567915
femkepiepers <43027739+femkepiepers@users.noreply.github.com>
2019-02-19 20:02:44 +0100

    Vier op een rij

--- 12b - Roosters/Vier op een rij.py
+++ 12b - Roosters/Vier op een rij.py
@@ -1,3 +1,16 @@
 def printbaar_rek(rek):
-
-    mes =
+    i = len(rek) - 1
+    a = 0
+    mes = ''
+    while i < len(rek):
+        while rek[i][a] == 0:
+            mes += 0
+            a += 1
+        while rek[i][a] == 'R':
+            mes += 'R'
+            a += 1
+        while rek[i][a] == 'G':
+            mes += 'G'
+            a += 1
+        i += -1
+    return mes
