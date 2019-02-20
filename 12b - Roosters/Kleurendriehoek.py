039efb5e5ce988213240a160b8673f701e2fb778
femkepiepers <43027739+femkepiepers@users.noreply.github.com>
2019-02-20 19:22:05 +0100

    Kleurendriehoek

--- 12b - Roosters/Kleurendriehoek.py
+++ 12b - Roosters/Kleurendriehoek.py
@@ -1,8 +1,48 @@
 def volgende_rij(rij):
-    lijst = []
-    i = 0
-    for i in range(0, len(lijst)):
+    next = []
+    for i in range(0, len(rij) - 1):
+
         if rij[i] == rij[i + 1]:
-            lijst.append('rij[i]')
+            next.append(rij[i])
+
         elif rij[i] != rij[i + 1]:
-            lijst.append('rij')
+            kleur = ['Y', 'R', 'G']
+            kleur.remove(rij[i])
+            kleur.remove(rij[i + 1])
+            next.append(kleur[0])
+
+    return next
+
+def driehoek(rij):
+    helemaal = []
+    i = 0
+    helemaal.append(rij)
+
+    while len(helemaal[i]) > 1:
+        next = volgende_rij(helemaal[i])
+        helemaal.append(next)
+        i += 1
+
+    return helemaal
+
+def kleuren(driehoek):
+    geel = 0
+    rood = 0
+    groen = 0
+
+    for i in range(len(driehoek)):
+
+        for a in range(len(driehoek[i])):
+
+            kleur = driehoek[i][a]
+            if kleur == 'G':
+                groen += 1
+
+            elif kleur == 'R':
+                rood += 1
+
+            elif kleur == 'Y':
+                geel += 1
+
+    return groen, rood, geel
+
--- 12b - Roosters/Vier op een rij.py
+++ 12b - Roosters/Vier op een rij.py
@@ -1,16 +1,1 @@
-def printbaar_rek(rek):
-    i = len(rek) - 1
-    a = 0
-    mes = ''
-    while i < len(rek):
-        while rek[i][a] == 0:
-            mes += 0
-            a += 1
-        while rek[i][a] == 'R':
-            mes += 'R'
-            a += 1
-        while rek[i][a] == 'G':
-            mes += 'G'
-            a += 1
-        i += -1
-    return mes
+def printbaar_rek(rij):
